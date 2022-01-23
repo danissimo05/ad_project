fcd13e651b037b06b02b7c9d14ba27ac84f4deb1
dan <danikmega123@gmail.com>
2022-01-23 15:24:10 +0300

    12

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router/index'
 import store from './store'
+import fb from 'firebase'
 
 Vue.use(Router)
 Vue.config.productionTip = false
@@ -12,5 +13,20 @@ new Vue({
 vuetify,
 render: h => h(App),
 router:router,
-store
+store,
+  created(){
+const firebaseConfig = {        
+        apiKey: "AIzaSyByv8YXingoj59CQQNEI8IMk6fHQt2s8h8",
+  authDomain: "adproject-f89e4.firebaseapp.com",
+  databaseURL: "https://adproject-f89e4-default-rtdb.firebaseio.com",
+  projectId: "adproject-f89e4",
+  storageBucket: "adproject-f89e4.appspot.com",
+  messagingSenderId: "424807203787",
+  appId: "1:424807203787:web:eedf1d0bd3c2cb6697ac4e"
+      };
+  // Initialize Firebase
+  fb.initializeApp(firebaseConfig);
+  fb.analytics();
+  }
+
 }).$mount('#app')
